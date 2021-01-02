754106443674d50d15d73124ad1df5a486932abf
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-02 21:25:07 +0800

    Create autoload.js

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -28,6 +28,7 @@ function loadExternalResource(url, type) {
 
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
+	//"768"为最低显示宽度，单位为像素
 	Promise.all([
 		loadExternalResource(live2d_path + "waifu.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
