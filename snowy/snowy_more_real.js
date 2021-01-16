507a58efa012065c6ae101f5e4709a40219d3a33
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-16 18:04:30 +0800

    Create snowy_more_real.js

--- snowy/snowy_more_real.js
+++ snowy/snowy_more_real.js
@@ -4,14 +4,14 @@
 			documentHeight = $(document).height(),
 			documentWidth = $(document).width(),
 			defaults = {
-				minSize	: 10,		//雪花的最小尺寸
-				maxSize	: 20,		//雪花的最大尺寸
-				newOn : 1000,		//雪花出现的频率
+				minSize	: 10,   //雪花的最小尺寸
+				maxSize	: 20,   //雪花的最大尺寸
+				newOn : 1000,   //雪花出现的频率
 				flakeColor : "#FFFFFF"			
 			},
 			options	= $.extend({}, defaults, options);
 		var interval = setInterval( function(){
-			var startPositionLeft 	= Math.random() * documentWidth - 100,
+			var startPositionLeft = Math.random() * documentWidth - 100,
 			    startOpacity = 0.5 + Math.random(),
 			    sizeFlake = options.minSize + Math.random() * options.maxSize,
 			    endPositionTop = documentHeight - 40,
