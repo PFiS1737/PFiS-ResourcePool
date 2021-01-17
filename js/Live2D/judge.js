cbdea78766b78449c9e8c3cbc29d7101df8e009c
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-17 15:42:23 +0800

    Create judge.js

--- js/Live2D/judge.js
+++ js/Live2D/judge.js
@@ -1,1 +1,8 @@
-
+(function showEruda() {
+    var live2d = localStorage.getItem('live2d');
+    if (live2d === 'false') {
+        self.location.href="javascript:localStorage.setItem('live2d','true')";
+    } else if (eruda === 'true') {
+        self.location.href="javascript:localStorage.setItem('live2d','false')";
+    }
+}
