71d7b9fac93fb1c06eab7092d6cbb5bbcb185c3d
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-17 15:39:56 +0800

    Create load.js

--- js/Eruda/load.js
+++ js/Eruda/load.js
@@ -5,12 +5,3 @@
         document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
     }
 })();
-
-(function showEruda() {
-    var eruda = localStorage.getItem('eruda');
-    if (eruda === 'false') {
-        self.location.href="javascript:localStorage.setItem('eruda','true')";
-    } else if (eruda === 'true') {
-        self.location.href="javascript:localStorage.setItem('eruda','false')";
-    }
-}
