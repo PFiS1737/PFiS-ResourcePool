09bcc64fcf11ca11f649d8dcb261acaeb01109a5
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-17 15:29:10 +0800

    Create start.js

---
+++ js/Eruda/start.js
@@ -0,0 +1,16 @@
+(function () {
+if (/eruda=true/.test(window.location) || localStorage.getItem('eruda') === 'true') {
+var src = '//cdn.jsdelivr.net/npm/eruda';
+document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
+}
+})();
+
+(function showEruda() {
+    var eruda = localStorage.getItem('eruda');
+    if (eruda === 'false') {
+        self.location.href="javascript:localStorage.setItem('eruda','true')";
+    } else if (eruda === 'true') {
+        self.location.href="javascript:localStorage.setItem('eruda','false')";
+    }
+}
