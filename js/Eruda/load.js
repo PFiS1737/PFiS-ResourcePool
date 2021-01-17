b28b4126b60cadb43623a6fe3aefe3d3141d2b46
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-17 15:31:38 +0800

    Create load.js

--- js/Eruda/load.js
+++ js/Eruda/load.js
@@ -1,9 +1,9 @@
 (function () {
-if (/eruda=true/.test(window.location) || localStorage.getItem('eruda') === 'true') {
-var src = '//cdn.jsdelivr.net/npm/eruda';
-document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
-document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
-}
+    if (/eruda=true/.test(window.location) || localStorage.getItem('eruda') === 'true') {
+        var src = '//cdn.jsdelivr.net/npm/eruda';
+        document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+        document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
+    }
 })();
 
 (function showEruda() {
