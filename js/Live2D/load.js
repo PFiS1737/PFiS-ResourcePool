c87e6353b122052257e6f4c130c6df8d998a7385
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-17 15:40:57 +0800

    Create load.js

--- js/Live2D/load.js
+++ js/Live2D/load.js
@@ -1,6 +1,6 @@
 (function () {
-        if (/live2d=true/.test(window.location) || localStorage.getItem('live2d') === 'true') {
-                var src = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/live2d/special/autoload_self-customize.js';
-                document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
-        }
+    if (/live2d=true/.test(window.location) || localStorage.getItem('live2d') === 'true') {
+        var src = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/live2d/special/autoload_self-customize.js';
+        document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+    }
 })();
