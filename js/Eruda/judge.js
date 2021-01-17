9442293bd227fedc3f293b21f4cc11fb96d25141
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-17 15:40:20 +0800

    Create judge.js

--- js/Eruda/judge.js
+++ js/Eruda/judge.js
@@ -1,1 +1,8 @@
-
+(function showEruda() {
+    var eruda = localStorage.getItem('eruda');
+    if (eruda === 'false') {
+        self.location.href="javascript:localStorage.setItem('eruda','true')";
+    } else if (eruda === 'true') {
+        self.location.href="javascript:localStorage.setItem('eruda','false')";
+    }
+}
