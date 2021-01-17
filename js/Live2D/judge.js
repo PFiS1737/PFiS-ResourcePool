dc8b9af40d6f30b4e816052071f873ddf9326870
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-17 17:25:22 +0800

    Create judge.js

--- js/Live2D/judge.js
+++ js/Live2D/judge.js
@@ -1,10 +1,13 @@
 (function showEruda() {
     var live2d = localStorage.getItem('live2d');
     if (live2d === 'false') {
-        self.location.href="javascript:localStorage.setItem('live2d','true')";
+        localStorage.setItem('live2d','right');
         location.reload();
-    } else if (eruda === 'true') {
-        self.location.href="javascript:localStorage.setItem('live2d','false')";
+    } else if (eruda === 'right') {
+        localStorage.setItem('live2d','left');
+        location.reload();
+    } else if (eruda === 'left') {
+        localStorage.setItem('live2d','false');
         location.reload();
     }
 }
