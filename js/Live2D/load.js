45c71a4a2510d2d49061422b93373e603f8ce8f1
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-17 17:33:23 +0800

    Create load.js

--- js/Live2D/load.js
+++ js/Live2D/load.js
@@ -1,9 +1,9 @@
 (function () {
-    if (/live2d-right=true/.test(window.location) || localStorage.getItem('live2d') === 'true') {
+    if (/live2d-right=true/.test(window.location) || localStorage.getItem('live2d') === 'right') {
         var src-right = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/live2d/special/autoload_self-customize.js';
         document.write('<scr' + 'ipt src="' + src-right + '"></scr' + 'ipt>');
-    } else if (/live2d-left=true/.test(window.location) || localStorage.getItem('live2d') === 'true') {
-        var src-left = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/live2d/special/autoload_self-customize.js';
+    } else if (/live2d-left=true/.test(window.location) || localStorage.getItem('live2d') === 'left') {
+        var src-left = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/live2d/special/autoload_self-customize_left.js';
         document.write('<scr' + 'ipt src="' + src-left + '"></scr' + 'ipt>');
     }
 })();
