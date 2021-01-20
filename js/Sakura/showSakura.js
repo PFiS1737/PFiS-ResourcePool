d165e4895aa35f039c5d44f02bde931eefdc03fb
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-20 23:58:33 +0800

    Create showSakura.js

--- js/Sakura/showSakura.js
+++ js/Sakura/showSakura.js
@@ -1,48 +1,57 @@
 (function SakuraLoad() {
-    if (localStorage.getItem('sakura') === 'native') {
-        var src = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/js/Sakura/Sakura-native.js';
+    if (localStorage.getItem('sakura') === 'lot') {
+        var src = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/js/Sakura/Sakura-lot.js';
+        document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+    } else if (localStorage.getItem('sakura') === 'more') {
+        var src = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/js/Sakura/Sakura-more.js';
         document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
     } else if (localStorage.getItem('sakura') === 'half') {
         var src = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/js/Sakura/Sakura-half.js';
         document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
-    } else if (localStorage.getItem('sakura') === 'quarter') {
-        var src = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/js/Sakura/Sakura-quarter.js';
-        document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
     } else if (localStorage.getItem('sakura') === 'less') {
         var src = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/js/Sakura/Sakura-less.js';
+        document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+    } else if (localStorage.getItem('sakura') === 'few') {
+        var src = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/js/Sakura/Sakura-few.js';
         document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
     }
 })();
 
 function SakuraJudge() {
     if (localStorage.getItem('sakura') === null) {
-        localStorage.setItem('sakura','native');
-        location.reload();
-    } else if (localStorage.getItem('sakura') === 'native') {
+        localStorage.setItem('sakura','lot');
+     //   location.reload();
+    } else if (localStorage.getItem('sakura') === 'lot') {
+        localStorage.setItem('sakura','more');
+      //  location.reload();
+    } else if (localStorage.getItem('sakura') === 'more') {
         localStorage.setItem('sakura','half');
-        location.reload();
+      //  location.reload();
     } else if (localStorage.getItem('sakura') === 'half') {
-        localStorage.setItem('sakura','quarter');
-        location.reload();
-    } else if (localStorage.getItem('sakura') === 'quarter') {
         localStorage.setItem('sakura','less');
-        location.reload();
+      //  location.reload();
     } else if (localStorage.getItem('sakura') === 'less') {
+        localStorage.setItem('sakura','few');
+      //  location.reload();
+    } else if (localStorage.getItem('sakura') === 'few') {
         localStorage.removeItem('sakura');
-        location.reload();
+       // location.reload();
     }
+    location.reload();
 };
 
 function SakuraWrite() {
     if (localStorage.getItem('sakura') === null) {
         document.write('关闭');
-    } else if (localStorage.getItem('sakura') === 'native') {
+    } else if (localStorage.getItem('sakura') === 'few') {
+        document.write('很多');
+    } else if (localStorage.getItem('sakura') === 'more') {
         document.write('较多');
     } else if (localStorage.getItem('sakura') === 'half') {
-        document.write('一半');
-    } else if (localStorage.getItem('sakura') === 'quarter') {
-        document.write('较少');
+        document.write('适中');
     } else if (localStorage.getItem('sakura') === 'less') {
+        document.write('较少');
+    } else if (localStorage.getItem('sakura') === 'few') {
         document.write('很少');
     }
 };
