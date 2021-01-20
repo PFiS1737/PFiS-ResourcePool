42b7102f0da39bf382a364fc37fd83d22360798d
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-20 22:26:44 +0800

    Create showLive2D.js

--- js/Live2D/showLive2D.js
+++ js/Live2D/showLive2D.js
@@ -1,37 +1,37 @@
 (function Live2DLoad() {
-    if (localStorage.getItem('live2d') === 'right') {
+    if (localStorage.getItem('live2d') === right) {
         var src = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/live2d/special/autoload_self-customize.js';
         document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
-    } else if (localStorage.getItem('live2d') === 'left') {
+    } else if (localStorage.getItem('live2d') === left) {
         var src = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/live2d/special/autoload_self-customize_left.js';
         document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
     }
 })();
 
 function Live2DJudge() {
-    if (localStorage.getItem('live2d') === 'null') {
+    if (localStorage.getItem('live2d') === null) {
         localStorage.setItem('live2d','right');
         location.reload();
-    } else if (localStorage.getItem('live2d') === 'false') {
+    } else if (localStorage.getItem('live2d') === false) {
         localStorage.setItem('live2d','right');
         location.reload();
-    } else if (localStorage.getItem('live2d') === 'right') {
+    } else if (localStorage.getItem('live2d') === right) {
         localStorage.setItem('live2d','left');
         location.reload();
-    } else if (localStorage.getItem('live2d') === 'left') {
+    } else if (localStorage.getItem('live2d') === left) {
         localStorage.setItem('live2d','false');
         location.reload();
     }
 };
 
 function Live2DWrite() {
-    if (localStorage.getItem('live2d') === 'null') {
+    if (localStorage.getItem('live2d') === null) {
         document.write('关');
-    } else if (localStorage.getItem('live2d') === 'false') {
+    } else if (localStorage.getItem('live2d') === false) {
         document.write('关');
-    } else if (localStorage.getItem('live2d') === 'right') {
+    } else if (localStorage.getItem('live2d') === right) {
         document.write('右');
-    } else if (localStorage.getItem('live2d') === 'left') {
+    } else if (localStorage.getItem('live2d') === left) {
         document.write('左');
     }
 };
