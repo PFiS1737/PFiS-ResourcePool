d51f4a5914a446d8a459890c4865f5bbac2f2b14
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-20 22:29:06 +0800

    Create showEruda.js

--- js/Eruda/showEruda.js
+++ js/Eruda/showEruda.js
@@ -1,5 +1,5 @@
 (function ErudaLoad() {
-    if (localStorage.getItem('eruda') === true) {
+    if (localStorage.getItem('eruda') === 'true') {
         var src = '//cdn.jsdelivr.net/npm/eruda';
         document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
         document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
@@ -10,10 +10,10 @@ function ErudaJudge() {
     if (localStorage.getItem('eruda') === null) {
         localStorage.setItem('eruda','true');
         location.reload();
-    } else if (localStorage.getItem('eruda') === false) {
+    } else if (localStorage.getItem('eruda') === 'false') {
         localStorage.setItem('eruda','true');
         location.reload();
-    } else if (localStorage.getItem('eruda') === true) {
+    } else if (localStorage.getItem('eruda') === 'true') {
         localStorage.setItem('eruda','false');
         location.reload();
     }
@@ -22,9 +22,9 @@ function ErudaJudge() {
 function ErudaWrite() {
     if (localStorage.getItem('eruda') === null) {
         document.write('关');
-    } else if (localStorage.getItem('eruda') === false) {
+    } else if (localStorage.getItem('eruda') === 'false') {
         document.write('关');
-    } else if (localStorage.getItem('eruda') === true) {
+    } else if (localStorage.getItem('eruda') === 'true') {
         document.write('开');
     }
 };
