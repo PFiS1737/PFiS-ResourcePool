81bce72a2591b6665a09992356940549640941fc
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-20 22:14:38 +0800

    Create showLive2D.js

--- js/Live2D/load.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    if (/live2d-right=true/.test(window.location) || localStorage.getItem('live2d') === 'right') {
-        var srcright = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/live2d/special/autoload_self-customize.js';
-        document.write('<scr' + 'ipt src="' + srcright + '"></scr' + 'ipt>');
-    } else if (/live2d-left=true/.test(window.location) || localStorage.getItem('live2d') === 'left') {
-        var srcleft = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/live2d/special/autoload_self-customize_left.js';
-        document.write('<scr' + 'ipt src="' + srcleft + '"></scr' + 'ipt>');
-    }
-})();
