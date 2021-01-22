06b56d93f9521341f87976765be0555761beb0c1
PFiS <69380732+PFiS1737@users.noreply.github.com>
2021-01-22 22:04:16 +0800

    Create showSakura.js

--- js/Sakura/showSakura.js
+++ js/Sakura/showSakura.js
@@ -1,35 +1,39 @@
 (function SakuraLoad() {
-    if (localStorage.getItem('sakura') != null) {
+    if (localStorage.getItem('SakuraNumber') != null) {
         var src = '//cdn.jsdelivr.net/gh/PFiS1737/PFiS_Public_Repository@latest/js/Sakura/Sakura.js';
         document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
     }
 })();
 
 function SakuraChange() {
-    if (localStorage.getItem('sakura') === null) {
-        localStorage.setItem('sakura','5');
-    } else if (localStorage.getItem('sakura') < 100) {
-        localStorage.setItem('sakura',Number(Number(localStorage.getItem('sakura')) + 5));
-    } else if (localStorage.getItem('sakura') === '100') {
-        localStorage.removeItem('sakura');
+    if (localStorage.getItem('SakuraNumber') === null) {
+        localStorage.setItem('SakuraNumber','5');
+    } else if (localStorage.getItem('SakuraNumber') < 100) {
+        localStorage.setItem('SakuraNumber',Number(Number(localStorage.getItem('SakuraNumber')) + 5));
+    } else if (localStorage.getItem('SakuraNumber') === '100') {
+        localStorage.removeItem('SakuraNumber');
     }
     location.reload();
 };
 
+function SakuraGetMax() {
+    localStorage.setItem('SakuraMax',Number(document.getElementsByName("SakuraMax").value));
+};
+
 (function SakuraChangeStop() {
-    if (localStorage.getItem('sakura') > 100) {
-        localStorage.removeItem('sakura');
+    if (localStorage.getItem('SakuraNumber') > localStorage.getItem('SakuraMax')) {
+        localStorage.removeItem('SakuraNumber');
     }
 })();
 
 function SakuraGetElements() {
-    localStorage.setItem('sakura',Number(document.getElementsByName("SakuraNumber").value));
+    localStorage.setItem('SakuraNumber',Number(document.getElementsByName("SakuraNumber").value));
 };
 
 function SakuraWrite() {
-    if (localStorage.getItem('sakura') === null) {
+    if (localStorage.getItem('SakuraNumber') === null) {
         document.write('关闭');
-    } else if (localStorage.getItem('sakura') !== null) {
-        document.write('数量：' + localStorage.getItem('sakura'));
+    } else if (localStorage.getItem('SakuraNumber') !== null) {
+        document.write('数量：' + localStorage.getItem('SakuraNumber'));
     }
 };
